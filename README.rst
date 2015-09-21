7fb7a6c354ca5f7f27a77c3d33079c66824e1eeb
Thomas Newman <tnewman1@ltu.edu>
2015-09-21 19:24:45 -0400

    Initial Project Skeleton

---
+++ README.rst
@@ -0,0 +1,15 @@
+PyKazoo
+=======
+
+.. image:: https://travis-ci.org/tnewman/KazooCommission.svg?branch=master
+  :target: https://travis-ci.org/tnewman/KazooCommission
+.. image:: https://coveralls.io/repos/tnewman/KazooCommission/badge.svg?branch=master&service=github
+  :target: https://coveralls.io/github/tnewman/KazooCommission?branch=master
+.. image:: https://readthedocs.org/projects/kazoocommission/badge/?version=latest
+  :target: https://readthedocs.org/projects/kazoocommission/?badge=latest
+
+PyKazoo is a Python API client for `2600hz Kazoo`_. `2600hz Kazoo`_ is an excellent, scalable VoIP (Voice over IP)
+platform, which exclusively uses a JSON API for configuration. The goal of PyKazoo is to provide an easy way for Python
+developers to interface with this excellent software to start building great applications.
+
+.. _`2600hz Kazoo`: https://2600hz.atlassian.net/wiki/display/docs/Overview
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# noinspection PyUnresolvedReferences
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['--pep8', '--cov']
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    cmdclass={'test': PyTest},
+    name='KazooCommission',
+    version='0.0a4',
+    packages=['kazoocommission'],
+    install_requires=['pykazoo>=0.0a4'],
+    tests_require=['pytest>=2.6.4', 'pytest-cov>=2.1.0', 'pytest-pep8>=1.0.6'],
+    url='https://github.com/tnewman/PyKazoo',
+    license='MIT',
+    author='Thomas Newman',
+    author_email='tnewman@users.noreply.github.com',
+    description='PyKazoo is a Python API client for 2600hz Kazoo',
+)
