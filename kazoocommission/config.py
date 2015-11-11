71dbfcfded4f5a44073b76f6ebd181dc16c6e550
Thomas Newman <tnewman1@ltu.edu>
2015-11-10 20:10:53 -0500

    Get configuration from environmental variables to prepare for Docker

--- kazoocommission/config.py
+++ kazoocommission/config.py
@@ -6,6 +6,7 @@
    environmental variables.
 """
 
+import os
 from enum import Enum
 
 
@@ -16,23 +17,26 @@ class DNSMode(Enum):
     ip = 3
 
 # Settings for Kazoo Commission Server
-KAZOO_COUCH_DB_URL = 'http://192.168.1.239:5984'
+KAZOO_COUCH_DB_URL = 'http://localhost:5984'
 """ URL to connect to CouchDB for 2600hz Kazoo """
 
-DEBUG = True
+DEBUG = 'KAZOO_COMMISSION_DEBUG' in os.environ
 """ Enable Server Debug Mode """
 
-SSL_CLIENT_SUBJECT_VALIDATION = False
+SSL_CLIENT_SUBJECT_VALIDATION = \
+    'KAZOO_COMMISSION_SSL_CLIENT_SUBJECT_VALIDATION' in os.environ
 """ Whether or not the server will validate the MAC address of the requested
     device against the the X-SSL-Subject header passed from the web server.
     Highly recommended for security. """
 
 # Settings for Template Generation
-SIP_OUTBOUND_PROXY = 'preproduction.cita-communications.net'
+SIP_OUTBOUND_PROXY = os.environ.get('KAZOO_COMMISSION_SIP_OUTBOUND_PROXY',
+                                    'localhost')
 
-SIP_DNS_SRV = True
-""" Use DNS SRV Mode """
+SIP_DNS_SRV = 'SIP_DNS_A_RECORD' in os.environ
+""" Use DNS A Record Mode - Defaults to DNS SRV Record if left unset """
 
-FIRMWARE_SERVER_URL = 'http://localhost'
+FIRMWARE_SERVER_URL = os.environ.get('FIRMWARE_SERVER_URL',
+                                     'http://localhost/firmware')
 """ URL of base directory firmware is served from (note: each template has
     its own subdirectory for firmware) """
