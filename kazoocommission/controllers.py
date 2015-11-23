e332943a237fc827afb0b20e6356b9c96bb86064
Thomas Newman <tnewman1@ltu.edu>
2015-11-23 15:09:04 -0500

    Add account data

--- kazoocommission/controllers.py
+++ kazoocommission/controllers.py
@@ -2,7 +2,7 @@ from flask import abort, Flask, make_response, render_template, request
 from functools import wraps
 from jinja2.exceptions import TemplateNotFound
 from kazoocommission import config
-from kazoocommission.services import KazooDeviceService
+from kazoocommission.services import KazooAccountService, KazooDeviceService
 
 app = Flask(__name__)
 
@@ -10,19 +10,32 @@ app = Flask(__name__)
 def authenticate(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
+        account_service = kwargs.get('account_service')
         device_service = kwargs.get('device_service')
 
+        kwargs.pop('account_service', None)
         kwargs.pop('device_service', None)
 
         mac_address_delimited = ':'.join(
             a+b for a, b in zip(kwargs['mac_address'][::2],
                                 kwargs['mac_address'][1::2])).lower()
 
+        if account_service is None:
+            account_service = KazooAccountService()
+
         if device_service is None:
             device_service = KazooDeviceService()
 
+        kwargs['account_data'] = account_service.get_account_by_name(
+            kwargs['account'])
+
+        if kwargs['account_data'] is None:
+            abort(404)
+
+        account_id = kwargs['account_data']['id']
+
         kwargs['device_data'] = device_service.get_device_by_mac_address(
-            kwargs['account'], mac_address_delimited)
+            account_id, mac_address_delimited)
 
         if kwargs['device_data'] is None:
             abort(404)
@@ -44,13 +57,14 @@ def authenticate(fn):
 @app.route('/<manufacturer>/<model>/<account>/<mac_address>.xml')
 @authenticate
 def get_provisioning_file(manufacturer, model, account, mac_address,
-                          device_data):
+                          account_data, device_data):
 
     template_path = manufacturer + '/' + model + '.xml'
 
     try:
         phone_config = render_template(template_path, config=config,
-                                       account=account, device=device_data,
+                                       account=account_data,
+                                       device=device_data,
                                        mac_address=mac_address)
         response = make_response(phone_config)
         response.headers['Content-Type'] = 'application/xml'
--- kazoocommission/services.py
+++ kazoocommission/services.py
@@ -2,7 +2,74 @@ from kazoocommission import config
 import couchdb.client
 
 
-class KazooDeviceService:
+class KazooBaseService:
+    def __init__(self, couch_client=None):
+        """ Base class to provide access to the Kazoo platform.
+
+        :param couch_client: The CouchDB client to use to interact with the
+                             Kazoo CouchDB database. The default is
+                             couchdb-python.
+        :type couch_client: couchdb.client.Server
+    """
+
+        self.couch_client = couch_client
+
+        if self.couch_client is None:
+            self.couch_client = couchdb.client.Server(
+                config.KAZOO_COUCH_DB_URL)
+
+    @staticmethod
+    def _remove_private_keys_from_couch_data(unfiltered_couch_data):
+        return {key: unfiltered_couch_data[key] for key in
+                unfiltered_couch_data.keys() if not key.startswith('pvt_')}
+
+    @staticmethod
+    def _get_kazoo_account_id_couch_db_format(account_id):
+        part_one = account_id[0:2]
+        part_two = account_id[2:4]
+        part_three = account_id[4:]
+        couch_account_id = 'account' + '/' + part_one + '/' + part_two + '/' \
+                           + part_three
+
+        return couch_account_id
+
+
+class KazooAccountService(KazooBaseService):
+    """ Provides access to Kazoo platform accounts.
+
+        :param couch_client: The CouchDB client to use to interact with the
+                             Kazoo CouchDB database. The default is
+                             couchdb-python.
+        :type couch_client: couchdb.client.Server
+    """
+
+    def __init__(self, couch_client=None):
+        KazooBaseService.__init__(self, couch_client)
+
+    def get_account_by_name(self, account_name):
+        """ Gets a specific account by name.
+
+        :param account_name: Account Name to retrieve a device from.
+        :return: Account Data
+        :type account_name: str
+        :rtype: dict
+        """
+
+        try:
+            account_list = list(self.couch_client['accounts'].view(
+                'accounts/listing_by_name',
+                key=account_name))
+            account_id = account_list[0]['id']
+            account_data = self.couch_client['accounts'][account_id]
+        except IndexError:
+            return None
+
+        account_data = self._remove_private_keys_from_couch_data(account_data)
+
+        return account_data
+
+
+class KazooDeviceService(KazooBaseService):
     """ Provides access to Kazoo platform Devices.
 
         :param couch_client: The CouchDB client to use to interact with the
@@ -12,16 +79,12 @@ class KazooDeviceService:
     """
 
     def __init__(self, couch_client=None):
-        self.couch_client = couch_client
+        KazooBaseService.__init__(self, couch_client)
 
-        if self.couch_client is None:
-            self.couch_client = couchdb.client.Server(
-                config.KAZOO_COUCH_DB_URL)
-
-    def get_device_by_mac_address(self, account_name, mac_address):
+    def get_device_by_mac_address(self, account_id, mac_address):
         """ Gets a specific device for an account by MAC address.
 
-        :param account_name: Account Name to retrieve a device from.
+        :param account_id: Account ID to retrieve a device from.
         :param mac_address: Device MAC address in lowercase, colon delimited
                format (57:fb:69:4a:f5:c5)
         :return: Device Configuration
@@ -29,64 +92,27 @@ class KazooDeviceService:
         :rtype: dict
         """
 
-        couch_account_id = self._get_couch_account_id_for_account_name(
-            account_name)
-
-        if couch_account_id is None:
-            return None
-
-        device_data = self._retrieve_device(couch_account_id, mac_address)
-
-        if not device_data:
-            return None
-
-        return device_data
-
-    def _get_couch_account_id_for_account_name(self, account_name):
-        try:
-            account_list = list(self.couch_client['accounts'].view(
-                'accounts/listing_by_name',
-                key=account_name))
-            account_id = account_list[0]['value']['account_id']
-        except IndexError:
-            return None
-
         couch_account_id = self._get_kazoo_account_id_couch_db_format(
             account_id)
 
-        return couch_account_id
+        device_data = self._retrieve_device(couch_account_id, mac_address)
 
-    @staticmethod
-    def _get_kazoo_account_id_couch_db_format(account_id):
-        part_one = account_id[0:2]
-        part_two = account_id[2:4]
-        part_three = account_id[4:]
-        couch_account_id = 'account' + '/' + part_one + '/' + \
-                           part_two + '/' + part_three
-
-        return couch_account_id
+        return device_data
 
     def _retrieve_device(self, account_id_couch, mac_address):
         try:
             device_id = list(self.couch_client[account_id_couch].view(
                 'devices/listing_by_macaddress', key=mac_address))[0]['id']
-            unfiltered_device_data = \
-                self.couch_client[account_id_couch][device_id]
+            device_data = self.couch_client[account_id_couch][device_id]
         except IndexError:
             return None
 
-        device_data = self._remove_private_keys_from_device(
-            unfiltered_device_data)
+        device_data = self._remove_private_keys_from_couch_data(device_data)
 
         device_data['line_display_text'] = self._get_line_display_text(
             device_data)
 
-        return unfiltered_device_data
-
-    @staticmethod
-    def _remove_private_keys_from_device(unfiltered_device_data):
-        return {key: unfiltered_device_data[key] for key in
-                unfiltered_device_data.keys() if not key.startswith('pvt_')}
+        return device_data
 
     @staticmethod
     def _get_line_display_text(device_data):
